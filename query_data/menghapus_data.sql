4fe2a307a7d93a91258ff40ca17e9cf20c69ba82
Veramanda <faidatun237@gmail.com>
2023-11-10 21:39:00 +0700

    menghapus data

---
+++ query_data/menghapus_data.sql
@@ -0,0 +1,15 @@
+-- Menghapus Data Pada Table Pelanggan yang Memiliki ID Pelanggan 8
+DELETE FROM pelanggan
+where id_pelanggan = 5;
+
+-- Menghapus Data Pada Table Transaksi yang Memiliki ID Transaksi 1
+DELETE FROM transaksi
+where id_transaksi = 1;
+
+-- Menghapus Data Pada Table Barang yang Memiliki ID Barang 3
+DELETE FROM barang
+where id_barang = 3;
+
+-- Menghapus Data Pada Table Kategori yang Memiliki ID Kategori 10
+DELETE FROM kategori
+where id_kategori = 10;
